0d183cffbbbfc3c0726cc3d4904d3aa90e331f23
Bfleischmann <boni.digitalnatives@gmail.com>
2012-11-29 09:21:48 +0100

    timelog patch

--- lib/redmine_scrum_hooks.rb
+++ lib/redmine_scrum_hooks.rb
@@ -10,8 +10,9 @@ module RedmineScrumPlugin
 
         begin
           if issue.is_task? && User.current.allowed_to?(:te_remaining_hours, time_entry.project) != nil
-            snippet += "<p><label for='remaining_hours'>#{l(:field_te_remaining_hours)} #{issue.remaining_hours}</label>"
+            snippet += "<p><label for='remaining_hours'>#{l(:field_te_remaining_hours)} </label>"
             snippet += text_field_tag('te_remaining_hours', time_entry.te_remaining_hours, :size => 6)
+            snippet += "#{l(:remaining_hours_on_issue)}: #{issue.remaining_hours}"
             snippet += '</p>'
           end
           return snippet
@@ -32,22 +33,11 @@ module RedmineScrumPlugin
 
         if issue.is_task? && User.current.allowed_to?(:te_remaining_hours, time_entry.project) != nil
           if params.include?("te_remaining_hours")
-            if time_entry.new_record?
-              remaining_hours = params[:te_remaining_hours].gsub(',','.').to_f
-              if remaining_hours != issue.remaining_hours.to_f
-                issue.journalized_update_attribute(:remaining_hours, (remaining_hours)) if time_entry.save
-              end
-            else
-              remaining_hours_diff = params[:te_remaining_hours].gsub(',','.').to_f - time_entry.te_remaining_hours.to_f
-              if remaining_hours_diff != 0
-                issue.journalized_update_attribute(:remaining_hours, (issue.remaining_hours.to_f + remaining_hours_diff)) if time_entry.save
-              end
-            end
             time_entry.te_remaining_hours = params[:te_remaining_hours].gsub(',','.').to_f
-            Rails.logger.debug("remaining hours: #{remaining_hours}")
-            Rails.logger.debug("remaining hours diff: #{remaining_hours_diff}")
-            Rails.logger.debug("time_entry hours: #{time_entry.te_remaining_hours}")
-            Rails.logger.debug("issue remaining hours: #{issue.remaining_hours}")
+            remaining_hours = params[:te_remaining_hours].gsub(',','.').to_f
+            if remaining_hours != issue.remaining_hours
+              issue.journalized_update_attribute(:remaining_hours, remaining_hours) if time_entry.save
+            end
           end
         end
       end

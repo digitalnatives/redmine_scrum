82f9c39de05738cc5dfc3ed9e4e923f23069a666
boni <boni@twine.hu>
2013-01-10 22:52:49 +0100

    using redmine right click functionality

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,7 @@ if Rails::VERSION::MAJOR < 3
 
   ActionController::Routing::Routes.draw do |map|
     map.connect  '/scrum_report/:project_id', :controller => 'scrum_report', :action => 'index' 
-    map.resources :scrum_report_time_entries, :collection => { :update_issue_status => :post }
-    # get  '/scrum_report_time_entries/:time_entry' => 'scrum_report_time_entries#show'
-    # put  '/scrum_report_time_entries/:time_entry' => 'scrum_report_time_entries#update'
-    # post '/scrum_report_time_entries'             => 'scrum_report_time_entries#create'
+    map.resources :scrum_report_time_entries
   end
 
 else
